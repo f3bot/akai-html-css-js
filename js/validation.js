1db36a1f10bc8bec280298f453e3bc6b8a6f4c57
f3bot <110943732+f3bot@users.noreply.github.com>
2022-10-21 14:49:05 +0200

    Update validation.js

--- js/validation.js
+++ js/validation.js
@@ -45,3 +45,27 @@ form.addEventListener('submit', (e) =>{
     }
 })
 
+// 1. odwróć liczbę
+// np dla 12345, funkcja powinna zwrócić 54321
+function reverseNumber(number) {
+  let x = number.toString().split("");
+  let xreversed = x.reverse().join("")
+  return xreversed
+}
+
+console.log("1.", reverseNumber(12345));
+
+// 2. doodaj do siebie wszystkie wartości z tablicy, które są parzyste
+// dla tablicy tab powinniśmy otrzymać 2 + 4 + 6 + 8 = 20
+const tab = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+function addEven(array) {
+  let sum = 0;
+  for(let i = 0; i <= array.length; i++){
+    if(tab[i] % 2 === 0){
+      sum += tab[i]
+    }
+  }
+  return sum
+}
+
+console.log("2.", addEven(tab));
